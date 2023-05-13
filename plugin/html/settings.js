c8db43e3087c9fbb1273858f9c5c3bfcde1a159d
Ali Raheem <ali-windows@shoryuken.me>
2023-05-13 17:21:38 +0100

    Not emails anymore

--- plugin/html/settings.js
+++ plugin/html/settings.js
@@ -8,12 +8,12 @@ document.addEventListener("DOMContentLoaded", function () {
     var maxTokensInput = document.getElementById("max-tokens");
     var defaultButton = document.getElementById("default-settings");
     var defaultActions = [
-        { name: "Reply to this", prompt: "Reply to the following email." },
+        { name: "Reply to this", prompt: "Reply to the following message." },
         { name: "Rewrite Polite", prompt: "Rewrite the following text to be more polite. Reply with only the re-written text and no extra comments." },
         { name: "Rewrite Formal", prompt: "Rewrite the following text to be more formal. Reply with only the re-written text and no extra comments." },
         { name: "Classify", prompt: "Classify the following text in terms of Politeness, Warmth, Formality, Assertiveness, Offensiveness giving a percentage for each category. Reply with only the category and score and no extra text." },
-        { name: "Summerize this", prompt: "Summerize the following email into a bullet point list." },
-        { name: "Translate this", prompt: "Translate the following email in English." },
+        { name: "Summerize this", prompt: "Summerize the following text into a bullet point list." },
+        { name: "Translate this", prompt: "Translate the following text in English." },
         { name: "Prompt provided", prompt: " " },
     ];
     var defaultModel = "gpt-3.5-turbo";
